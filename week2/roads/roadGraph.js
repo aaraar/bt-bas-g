435b45000911cfed70eec49e2e9a8d5cc7187c3e
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2019-04-25 16:12:23 +0200

    cleaned up roadGraph object return

--- week2/roads/roadGraph.js
+++ week2/roads/roadGraph.js
@@ -1,7 +1,9 @@
 const buildGraph = require('./graph');
 
 const roadGraph = (roads) => {
-    let roadsArrays = roads.map(road => road.split('-'));
+    // The expression below splits the items into seperate strings inside an array, but also replaces the ' ' and "'" to sanitize the return object
+    let roadsArrays = roads.map( road => road.split('-').map(road => road.replace(' ', '_').replace("'", '')) );
+
     return buildGraph(roadsArrays);
 };
 
